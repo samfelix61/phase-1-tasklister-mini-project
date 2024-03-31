10985c094907b2e86ed4c06e5584a2c4a76bfce4
samfelix61 <samfelix61@gmail.com>
2024-03-31 18:27:56 +0300

    form assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+const newForm = document.querySelector("form");
+newForm.addEventListener("submit", function(e){
+      e.preventDefault();
+
+      const toDo = e.target.querySelector("#new-task-description").value;
+      addToDo(toDo);
+      newForm.reset()
+ })
+})
+function addToDo(toDo){
+ const theLi = document.createElement('li');
+ const btn = document.createElement('button')
+ btn.textContent = 'x'  
+ theLi.textContent = toDo
+ theLi.appendChild(btn);  
+ document.querySelector('#tasks').appendChild(theLi)
+ btn.addEventListener('click', handleDelete)
+}
+function handleDelete(e){
+ e.target.parentNode.remove()
+}
